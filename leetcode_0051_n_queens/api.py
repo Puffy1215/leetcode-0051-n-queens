4bf0fd03c8ac6a4bd17153089cd9d64ecdcd0bd2
Andy Xie <andy.xie.opensource@gmail.com>
2023-12-09 20:19:32 +0800

    api.py: Specify arguments

--- leetcode_0051_n_queens/api.py
+++ leetcode_0051_n_queens/api.py
@@ -1,12 +1,13 @@
 """API for solving problem N-Queens"""
 
-def _check_preconditions(...) -> bool:
+
+def _check_preconditions(n: int) -> bool:
     pass
 
 
-def n_queens(...) -> ...:
+def n_queens(n: int) -> list[list[str]]:
     """Solves problem N-Queens"""
 
-    assert _check_preconditions(...)
+    assert _check_preconditions(n)
 
     pass
