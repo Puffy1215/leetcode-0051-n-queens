fde85946bc940d6c70f096afd36372f3a521943e
Andy Xie <andy.xie.opensource@gmail.com>
2023-12-19 21:23:58 +0800

    api.py: Set queen's position

--- leetcode_0051_n_queens/api.py
+++ leetcode_0051_n_queens/api.py
@@ -10,8 +10,26 @@ def _check_preconditions(n: int) -> bool:
     return N_MIN <= n <= N_MAX
 
 
-def _recursive_n_queens(board: list[str]) -> Iterator[list[str]]:
-    pass
+GOOD = " "
+BAD = "."
+QUEEN = "Q"
+
+
+def _set_queen(board: list[list[str]], x: int, y: int) -> None:
+    n = len(board)
+    assert 0 <= x < n
+    assert 0 <= y < n
+    assert board[y][x] == GOOD
+
+    for i in range(n):
+        board[y][i] = BAD
+        board[i][x] = BAD
+        board[min(x+i,n)][min(y+i,n)] = BAD
+        board[max(x-i,0)][max(y-i,0)] = BAD
+        board[min(x+i,n)][max(y-i,0)] = BAD
+        board[max(x-i,0)][min(y+i,n)] = BAD
+
+    board[y][x] = QUEEN
 
 
 def n_queens(n: int) -> Iterator[list[str]]:
