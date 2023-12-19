713ef19b7fd66271ef8c31dac6c6f5b9933a8b20
Andy Xie <andy.xie.opensource@gmail.com>
2023-12-19 20:41:17 +0800

    test_api.py: Create test board

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,14 +5,43 @@ import pytest
 from leetcode_0051_n_queens import api
 
 
+def _to_list(board: str) -> list[list[str]]:
+    rows = board.split("\n")
+    rows = [r.strip() for r in rows]
+    rows = [r for r in rows if r]
+    return [list(r) for r in rows]
+
+
+_BOARDS_4 = [
+    """
+    .Q..
+    ...Q
+    Q...
+    ..Q.
+    """,
+    """
+    ..Q.
+    Q...
+    ...Q
+    .Q..
+    """,
+]
+
+_BOARDS_1 = [
+    """
+    Q
+    """,
+]
+
+
 @pytest.mark.parametrize(
     ["result", "n"],
     (
-        [[[".Q..", "...Q", "Q...", "..Q."], ["..Q.", "Q...", "...Q", ".Q.."]], 4],
-        [[["Q"]], 1],
+        [[_to_list(b) for b in _BOARDS_4], 4],
+        [[_to_list(b) for b in _BOARDS_1], 1],
     ),
 )
-def test_n_queens(result: list[list[str]], n: int) -> None:
+def test_n_queens(result: list[list[list[str]]], n: int) -> None:
     """Tests solution for problem N-Queens"""
 
-    assert api.n_queens(n) == result
+    assert list(api.n_queens(n)) == result
